9cb0d69e81cdb04e2ff70a49e7d8629bb44057b7
Nika Kakulia <nikathedoodler@mac.lan>
2025-09-28 22:55:04 +0400

    filter and sorting finished

--- components/ui/SortModal.tsx
+++ components/ui/SortModal.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const SortModal = ({ sortModalOpen }: { sortModalOpen: boolean }) => {
-  console.log(sortModalOpen, "sortModalOpen");
+const SortModal = ({
+  sortModalOpen,
+  handleSort,
+}: {
+  sortModalOpen: boolean;
+  handleSort: (sort: string) => void;
+}) => {
   if (!sortModalOpen) {
     return null;
   }
@@ -9,13 +14,31 @@ const SortModal = ({ sortModalOpen }: { sortModalOpen: boolean }) => {
     <div className="text-[16px] flex flex-col absolute top-full right-0 z-10 gap-2 bg-[#FFFFFF] border border-[#E1DFE1] rounded-[8px] p-4 w-[223px] h-[184]">
       <div className="font-semibold text-[16px]">Sort by</div>
       <div className="flex flex-col gap-2 items-start">
-        <button className="cursor-pointer text-[16px]">
+        <button
+          className="cursor-pointer text-[16px]"
+          value="created_at"
+          onClick={() => {
+            handleSort("created_at");
+          }}
+        >
           New products first
         </button>
-        <button className="cursor-pointer text-[16px]">
+        <button
+          className="cursor-pointer text-[16px]"
+          value="price"
+          onClick={() => {
+            handleSort("price");
+          }}
+        >
           Price, low to high
         </button>
-        <button className="cursor-pointer text-[16px]">
+        <button
+          className="cursor-pointer text-[16px]"
+          value="-price"
+          onClick={() => {
+            handleSort("-price");
+          }}
+        >
           Price, high to low
         </button>
       </div>
