00807a876fcc12607296d1954eac1eb44aa53b35
Nika Kakulia <nikathedoodler@mac.lan>
2025-10-03 15:02:58 +0400

    cart ui is finished with continue shopping functionality

--- components/ui/Cart.tsx
+++ components/ui/Cart.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import CartLogo from "../icons/CartLogo";
+import CloseButton from "../icons/CloseButton";
+import Link from "next/link";
 
 type CartProps = {
   isCartOpen: boolean;
@@ -22,11 +24,34 @@ const Cart = ({ isCartOpen, setIsCartOpen, className }: CartProps) => {
 
       {/* Cart Sidebar */}
       <div
-        className={`fixed top-0 right-0 h-full w-full sm:w-96 lg:w-[500px] xl:w-[600px] bg-white shadow-2xl z-50 transform transition-transform duration-300 ease-in-out ${
+        className={`fixed top-0 right-0 h-full w-full sm:w-96 flex flex-col gap-30 lg:w-[500px] xl:w-[600px] bg-white shadow-2xl z-50 transform transition-transform duration-300 ease-in-out ${
           isCartOpen ? "translate-x-0" : "translate-x-full"
         } ${className}`}
       >
-        <CartLogo />
+        <div className="flex justify-between items-center p-4 mt-4">
+          <h1 className="text-[20px] font-[500] text-[#10151F]">
+            Sopping Cart ( {} )
+          </h1>
+          <CloseButton onClick={() => setIsCartOpen(false)} />
+        </div>
+        <div className="flex flex-col items-center justify-center gap-10">
+          <CartLogo />
+          <div className="flex flex-col items-center justify-center gap-2">
+            <div className="text-[24px] font-[600] text-[#10151F]">Ooops!</div>
+            <div className="text-[14px] font-[400] text-[#10151F]">
+              You’ve got nothing in your cart just yet...
+            </div>
+          </div>
+          <Link
+            href="/products"
+            className="w-1/3 sm:w-1/2 lg:w-2/5"
+            onClick={() => setIsCartOpen(false)}
+          >
+            <button className="w-full py-4 bg-[#FF4000] text-[#FFFFFF] text-base sm:text-lg lg:text-xl rounded-xl cursor-pointer hover:bg-[#E63900] transition-colors duration-200">
+              Start Shopping
+            </button>
+          </Link>
+        </div>
       </div>
     </>
   );
