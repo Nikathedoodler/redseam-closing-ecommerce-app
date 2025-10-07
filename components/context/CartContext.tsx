656dd9dccd4bff3d7a7b8d47888bd3a8d39aa9f2
Nika Kakulia <nikathedoodler@mac.lan>
2025-10-07 19:47:47 +0400

    created context file

---
+++ components/context/CartContext.tsx
@@ -0,0 +1,124 @@
+import { createContext, useState, useContext, ReactNode } from "react";
+
+type CartItem = {
+  id: number;
+  cover_image: string;
+  name: string;
+  price: number;
+  selectedQuantity: number;
+  maxStock: number;
+  total_price?: number;
+  size?: string;
+  color?: string;
+};
+
+type CartContextType = {
+  cartItems: CartItem[];
+  isCartOpen: boolean;
+  totalItems: number;
+  totalPrice: number;
+  addToCart: (item: CartItem) => void;
+  removeFromCart: (item: CartItem) => void;
+  clearCart: () => void;
+  setIsCartOpen: (isCartOpen: boolean) => void;
+  incrementQuantity: (itemId: number, maxStock: number) => void;
+  decrementQuantity: (itemId: number) => void;
+};
+
+const ProductCartContext = createContext<CartContextType | undefined>(
+  undefined
+);
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const totalItems = cartItems.reduce(
+    (sum, item) => sum + item.selectedQuantity,
+    0
+  );
+  const totalPrice = cartItems.reduce(
+    (sum, item) => sum + item.price * item.selectedQuantity,
+    0
+  );
+
+  const addToCart = (newItem: CartItem) => {
+    setCartItems((prev) => {
+      const existingItem = prev.find((item) => item.id === newItem.id);
+
+      if (existingItem) {
+        return prev.map((item) =>
+          item.id === newItem.id
+            ? {
+                ...item,
+                selectedQuantity:
+                  item.selectedQuantity + newItem.selectedQuantity,
+              }
+            : item
+        );
+      } else {
+        return [...prev, newItem];
+      }
+    });
+    setIsCartOpen(true);
+  };
+
+  const removeFromCart = (cartItem: CartItem) => {
+    setCartItems((prev) =>
+      prev.length ? prev.filter((item) => cartItem.id !== item.id) : prev
+    );
+  };
+
+  const incrementQuantity = (itemId: number, maxStock: number) => {
+    setCartItems((prev) =>
+      prev.map((item) => {
+        if (item.id === itemId) {
+          if (maxStock && item.selectedQuantity >= maxStock) {
+            return item;
+          }
+          return { ...item, selectedQuantity: item.selectedQuantity + 1 };
+        }
+        return item;
+      })
+    );
+  };
+
+  const decrementQuantity = (itemId: number) => {
+    setCartItems((prev) =>
+      prev
+        .map((item) => {
+          if (item.id === itemId) {
+            if (item.selectedQuantity <= 1) {
+              return undefined; // Remove item if quantity becomes 0
+            }
+            return { ...item, selectedQuantity: item.selectedQuantity - 1 };
+          }
+          return item;
+        })
+        .filter((item): item is CartItem => item !== undefined)
+    );
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  const contextValue: CartContextType = {
+    cartItems,
+    isCartOpen,
+    totalItems,
+    totalPrice,
+    addToCart,
+    removeFromCart,
+    clearCart,
+    setIsCartOpen,
+    incrementQuantity,
+    decrementQuantity,
+  };
+
+  return (
+    <ProductCartContext.Provider value={contextValue}>
+      {children}
+    </ProductCartContext.Provider>
+  );
+};
