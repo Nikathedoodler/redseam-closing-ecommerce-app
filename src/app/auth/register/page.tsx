9630941ed2f5f33162683adcb2d51a21153212e6
Nika Kakulia <nikathedoodler@mac.lan>
2025-10-11 21:11:35 +0400

    fixed image in registration and login

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -3,27 +3,35 @@ import React from "react";
 
 const register = () => {
   return (
-    <div className="mt-30 flex overscroll-none">
-      <div className="hidden lg:block lg:w-1/2">
-        <img src="/images/loginImage.jpeg" alt="" className="object-cover" />
+    <div className="flex h-screen">
+      <div className="hidden lg:block lg:w-1/2 h-full overflow-hidden">
+        <img
+          src="/images/loginImage.jpeg"
+          alt=""
+          className="w-full h-full object-cover object-[50%_-20%]"
+        />
       </div>
-      <div className="w-full md:w-2/3 lg:w-1/2 m-auto flex flex-col px-20 py-30 gap-10">
+      <div className="w-full md:w-2/3 lg:w-1/2 m-auto flex flex-col px-20 py-10 gap-10">
         <h1 className="text-3xl">Registration</h1>
         <div className="flex flex-col gap-6">
           <input
             placeholder="Username"
+            type="text"
             className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]"
           />
           <input
             placeholder="Email"
+            type="email"
             className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]"
           />
           <input
             placeholder="Password"
+            type="password"
             className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]"
           />
           <input
             placeholder="Confirm Password"
+            type="password"
             className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]"
           />
         </div>
