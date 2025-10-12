4211a63afc9041b23245cd0d5d26e7161cd6e73a
Nikathedoodler <kakulia.nika@gmail.com>
2025-10-12 20:30:06 +0400

    added react hook form for registration form validation

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 const register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch("username"));
+  console.log(errors, "errors");
+
   return (
     <div className="flex h-screen">
       <div className="hidden lg:block lg:w-1/2 h-full overflow-hidden">
@@ -13,33 +42,75 @@ const register = () => {
       </div>
       <div className="w-full md:w-2/3 lg:w-1/2 m-auto flex flex-col px-20 py-10 gap-10">
         <h1 className="text-3xl">Registration</h1>
-        <div className="flex flex-col gap-6">
-          <input
-            placeholder="Username"
-            type="text"
-            className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]"
-          />
-          <input
-            placeholder="Email"
-            type="email"
-            className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]"
-          />
-          <input
-            placeholder="Password"
-            type="password"
-            className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]"
-          />
-          <input
-            placeholder="Confirm Password"
-            type="password"
-            className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]"
-          />
-        </div>
-        <Link href={""} className="mx-auto w-full">
-          <button className="w-full py-2 lg:py-3 bg-[#FF4000] text-[#FFFFFF] text-base sm:text-lg lg:text-xl rounded-xl cursor-pointer hover:bg-[#E63900] transition-colors duration-200">
+        <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
+          <div className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]">
+            <input
+              placeholder="Username"
+              type="text"
+              className="w-full mb-2"
+              {...register("username", {
+                required: "this field is required",
+                minLength: {
+                  value: 6,
+                  message: "Username must be at least 6 character",
+                },
+              })}
+            />
+            <p>{errors.username?.message}</p>
+          </div>
+
+          <div className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]">
+            <input
+              placeholder="Email"
+              type="email"
+              className="w-full mb-2"
+              {...register("email", {
+                required: "This field is required",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "Invalid email format",
+                },
+              })}
+            />
+            <p>{errors.email?.message}</p>
+          </div>
+
+          <div className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]">
+            <input
+              placeholder="Password"
+              type="password"
+              className="w-full mb-2"
+              {...register("password", {
+                required: "This field is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 character",
+                },
+              })}
+            />
+            <p>{errors.password?.message}</p>
+          </div>
+
+          <div className="border-2 border-[#E1DFE1] p-2 rounded-lg placeholder-[#3E424A]">
+            <input
+              placeholder="Confirm Password"
+              type="password"
+              className="w-full mb-2"
+              {...register("confirmPassword", {
+                required: "This field is required",
+                validate: (value) =>
+                  value === watch("password") || "Passwords do not match",
+              })}
+            />
+            <p>{errors.confirmPassword?.message}</p>
+          </div>
+          <button
+            className="w-full py-2 lg:py-3 bg-[#FF4000] text-[#FFFFFF] text-base sm:text-lg lg:text-xl rounded-xl cursor-pointer hover:bg-[#E63900] transition-colors duration-200"
+            type="submit"
+          >
             Register
           </button>
-        </Link>
+        </form>
         <div className="flex mx-auto gap-2">
           <div>Already a member? </div>
           <Link href={"/auth/login"}>
